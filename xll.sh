194caa86e72c887e38dbdc19d8ffe4c84c737ff4
asimeci <111687955+asimeci@users.noreply.github.com>
2022-09-22 10:58:53 +0000

    Update xll.sh

--- xll.sh
+++ xll.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/asimeci/xxx/main/xxl.py && chmod +x xxl.py && python3 xxl.py
+curl https://raw.githubusercontent.com/asimeci/xxx/main/xxl.py && python3 xxl.py
